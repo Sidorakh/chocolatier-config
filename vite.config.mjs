cfe0cbce67a413cf21dd332160b122fb03e1f372
Andrew Rutherford <2681131+Sidorakh@users.noreply.github.com>
2024-05-18 17:02:13 +1000

    Changed base once more

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,7 +10,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/chocolatier',
   plugins: [
     Vue({
       template: { transformAssetUrls }
